474bc9b386f04fe6e820ac3acfaf9ff104b177de
Patryk <patryk.choma@gmail.com>
2020-03-23 10:47:43 +0100

    updated to use xpaths file

--- poste.py
+++ poste.py
@@ -2,6 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -9,50 +10,54 @@ import time
 import os
 import postetools
 
-login_page = "https://idp-bpiol.poste.it/jod-idp-bpiol/cas/login.html"
+LOGIN_PAGE = "https://idp-bpiol.poste.it/jod-idp-bpiol/cas/login.html"
 
 
 
 
 if __name__ == "__main__":
+    AZIENDA, USERNAME, PASSWORD = postetools.get_credentials()
+    XPATHS = postetools.get_xpaths()
     
     gecko = os.path.normpath(os.path.join(os.path.dirname(__file__), "geckodriver.exe"))
 
+    # headless options, uncomment for headless
+    # options = Options()
+    # options.headless = True
+    # driver = webdriver.Firefox(executable_path=gecko, options=options)
+
+    # headed for dev
     driver = webdriver.Firefox(executable_path=gecko)
-    driver.get(login_page)
-    #print("got page")
-    
+
+    # Get login page and wait for it to load
+    driver.get(LOGIN_PAGE)    
     wait = WebDriverWait(driver, 30)
     wait.until(EC.visibility_of_element_located((By.ID, "azienda")))
-
-    #Login page
     
+    # Find forms to fill
     form_azienda = driver.find_element_by_id("azienda")
-    #print("found form: azienda")
     form_username = driver.find_element_by_id("username")
-    #print("found form: username")
     form_pwd = driver.find_element_by_id("password")
-    #print("found form: password")
     time.sleep(2)
     
+    # Fill forms (clear them to be safe)
     form_azienda.clear()
-    form_azienda.send_keys("m000003842")
+    form_azienda.send_keys(AZIENDA)
     form_username.clear()
-    form_username.send_keys("CHMPRY91M12H501N")
+    form_username.send_keys(USERNAME)
     form_pwd.clear()
-    form_pwd.send_keys("BPIOL.patryk4")
+    form_pwd.send_keys(PASSWORD)
     #print("Sent all keys")
     time.sleep(2)
-
     form_pwd.send_keys(Keys.RETURN)
     time.sleep(4)
 
     # Premere continua
-    continua_login_btn = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div/div/div/div[3]/div/div/div/div/div/div[2]/div/div/div/div[2]/div/div/div/p/button")
+    continua_login_btn = driver.find_element_by_xpath(XPATHS["continua"])
     continua_login_btn.click()
 
     # Lista condomini
-    tabella_condomini = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/div/div/div/div[2]/div/div/div/div/div/div/div/div/div/form/div/table")
+    tabella_condomini = driver.find_element_by_xpath(XPATHS["tabella_condomini"])
     time.sleep(2)
     link_condomini_diz = postetools.extract_links_from_tabella_condomini(tabella_condomini)
     # for x in link_condomini_diz.items():
@@ -62,3 +67,17 @@ if __name__ == "__main__":
     # 1. download each item's bank account movements
     # 2. download each item's 896 report (carefully choose starting date for these 2 items)
     # 3. print(?) the bank account movements
+    for x in range(len(link_condomini_diz.keys())):
+        # Seleziona il condominio
+        print("Logging into: " + list(link_condomini_diz.keys())[x])
+        time.sleep(2)
+        link_condomini_diz[list(link_condomini_diz.keys())[x]].click()
+        time.sleep(2)
+        cambio_azienda = driver.find_element_by_xpath(XPATHS["cambio_azienda"])
+        cambio_azienda.click()
+        
+        #Re-get everything
+        time.sleep(4)
+        tabella_condomini = driver.find_element_by_xpath(XPATHS["tabella_condomini"])
+        link_condomini_diz = postetools.extract_links_from_tabella_condomini(tabella_condomini)
+        ###################
--- postetools.py
+++ postetools.py
@@ -1,4 +1,4 @@
-def extract_links_from_tabella_condomini(tabella_condomini):
+def extract_links_from_tabella_condomini(tabella_condomini) -> dict:
     '''
     tabella_condomini is a WebElement representation of a table
     returns a {"name": clickable_element} dict
@@ -10,3 +10,27 @@ def extract_links_from_tabella_condomini(tabella_condomini):
         clickable_elements.append(element)
     name_click_diz = {n.text:c for (n,c) in zip(clickable_elements[1:len(clickable_elements):2], clickable_elements[0:-1:2])}
     return name_click_diz
+
+def get_credentials() -> (str, str, str):
+    '''
+    needs a .credentials file in the directory
+    returns (azienda, username, password)
+    '''
+    with open(".credentials", "r") as f:
+        contents = f.read()
+        creds = contents.splitlines()
+    return creds[0], creds[1], creds[2]
+
+def get_xpaths() -> dict:
+    '''
+    needs a .xpaths file in the directory
+    returns a dictionary of xpaths
+    '''
+    #TODO: find some way to document the existing xpaths
+    xpaths = {}
+    with open(".xpaths", "r") as f:
+        contents = f.read()
+        for line in contents.splitlines():
+            name, path = line.split()
+            xpaths[name] = path
+    return xpaths
