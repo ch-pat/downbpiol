cd29332ae844dac96c166ee0c50dd7974e8029c6
Patryk Choma <patryk.choma@gmail.com>
2024-07-11 12:32:08 +0200

    fix for lowres

--- application/gui/layouts.py
+++ application/gui/layouts.py
@@ -10,7 +10,7 @@ def main_layout(condo_dict: dict) -> list:
         [sg.Frame("Seleziona i tracciati da scaricare", layout=[[sg.Col(layout=[[sg.Checkbox("896", default=True, key=("896", x))] + 
                                                                                 [sg.Checkbox("CBI", default=True, key=("CBI", x))] + 
                                                                                 [sg.T(x, pad=(5, 0))] for x in condo_dict.keys()],
-                                                                                scrollable=True, vertical_scroll_only=True, size=(650, 700))]])
+                                                                                scrollable=True, vertical_scroll_only=True, size=(650, max_height()))]])
         ],
         [sg.ProgressBar(100, orientation="horizontal", style="winnative", key="-BAR-", size=(20, 20)), sg.T("In attesa...", key="-MESSAGE-", size=(40, 2), font="Consolas 12")],
         [sg.Button("Scarica selezionati", key="-SCARICA-")]
@@ -35,3 +35,12 @@ def permute_checkboxes(window: sg.Window, values: dict):
     checkboxes = [window[key] for key in values if isinstance(key, tuple) and key[0] in ("896", "CBI")]
     for cb in checkboxes:
         cb.update(new_value)
+
+def max_height():
+    temp_window = sg.Window("tmp", [[]], alpha_channel=0, finalize=True)
+    _, h = temp_window.get_screen_dimensions()
+    temp_window.close()
+    del temp_window
+    if h < 900:
+        return 500
+    return 700
